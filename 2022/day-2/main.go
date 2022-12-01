62401e3d03baa6ff82f69bf5d71e17075a4aefde
Ethan Woodward <Ethan.Woodward@gmail.com>
2022-12-01 16:33:05 -0500

    rework scaffolding + only scaffold next day

--- 2022/day-2/main.go
+++ 2022/day-2/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var inputFileFlag = flag.String("inputFile", "sample.txt", "Relative file path to use as input.")
+
 func main() {
+	flag.Parse()
+	inputFile := *inputFileFlag
+
+    fmt.Println(inputFile)
 }
--- create-day-folder.sh
+++ create-day-folder.sh
@@ -16,7 +16,18 @@ mkdir day-$DAY
     cat << EOF > main.go
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
+var inputFileFlag = flag.String("inputFile", "sample.txt", "Relative file path to use as input.")
+
 func main() {
+	flag.Parse()
+	inputFile := *inputFileFlag
+
+    fmt.Println(inputFile)
 }
 EOF
 )
