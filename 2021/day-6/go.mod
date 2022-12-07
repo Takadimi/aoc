a42014e8f87bf5e1d03eefd2093249d2fb3789ca
Ethan Woodward <Ethan.Woodward@gmail.com>
2022-12-07 08:01:25 -0500

    finish day-6 2021
    Effectively copied liz fong jones for part 2. Original solution just
    couldn't scale to handling that many entries and would either time out
    or the process would get killed.
    
    Instead of the brute force approach, the much more elegant approach of
    storing the count of fish at each day trivialized the problem.

--- 2021/day-6/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Takadimi/aoc-2021-day-6
-
-go 1.18
--- 2021/day-6/main.go
+++ 2021/day-6/main.go
@@ -1,130 +1,135 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
-	"sync"
-	"sync/atomic"
-	"time"
+
+	"github.com/Takadimi/aoc/2021/file"
 )
 
-var inputFile = flag.String("inputFile", "sample1.txt", "Relative file path to use as input.")
-
-func lines(fileName string) ([]string, error) {
-	f, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-	result := []string{}
-	for scanner.Scan() {
-		line := scanner.Text()
-		result = append(result, line)
-	}
-
-	return result, nil
-}
-
-func parseAges(line string) (ages []uint8, err error) {
-	parts := strings.Split(line, ",")
-	for _, p := range parts {
-		age, err := strconv.Atoi(p)
-		if err != nil {
-			return ages, err
-		}
-		ages = append(ages, uint8(age))
-	}
-
-	return ages, nil
-}
-
-func evaluateOneDay(ages []uint8) (newAges []uint8) {
-	var wg sync.WaitGroup
-	workerCount := 15
-
-	var newFishCount uint64
-	now := time.Now()
-
-	for w := 0; w < workerCount; w++ {
-		wg.Add(1)
-		w := w
-		go func() {
-			defer wg.Done()
-			fmt.Println("Starting at ", w)
-			var nfc uint64
-			for i := w; i < len(ages); i += workerCount {
-				a := ages[i]
-				newAge := a - 1
-				if a == 0 {
-					newAge = 6
-					nfc++
-				}
-
-				ages[i] = newAge
-			}
-			atomic.AddUint64(&newFishCount, nfc)
-		}()
-	}
-	wg.Wait()
-
-	newFish := []uint8{}
-	for i := 0; i < int(newFishCount); i++ {
-		newFish = append(newFish, 8)
-	}
-
-	// for i := range ages {
-	// 	newAge := ages[i] - 1
-	// 	if newAge < 0 {
-	// 		newAge = 6
-	// 		newFish = append(newFish, 8)
-	// 	}
-	//
-	// 	ages[i] = newAge
-	// }
-	postFishEval := time.Now()
-	fmt.Printf("fish age eval: %s\n", postFishEval.Sub(now))
-
-	allFishAges := append(ages, newFish...)
-	fmt.Printf("append: %s\n", time.Now().Sub(postFishEval))
-	return allFishAges
-}
-
-func fishCountAfterDay(day int, ages []uint8) int {
-	for i := 0; i < day; i++ {
-		fmt.Printf("Day %d\n", i+1)
-		ages = evaluateOneDay(ages)
-	}
-	return len(ages)
-}
+var inputFileFlag = flag.String("inputFile", "sample.txt", "Relative file path to use as input.")
 
 func main() {
 	flag.Parse()
+	inputFile := *inputFileFlag
 
-	lines, err := lines(*inputFile)
+	lines, err := file.Lines(inputFile)
 	if err != nil {
-		fmt.Printf("failed to get lines for input file: %s\n", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	if len(lines) != 1 {
-		fmt.Printf("expect only on line in input file: %s\n", err)
-		os.Exit(1)
-	}
-
-	ages, err := parseAges(lines[0])
+	initialNumbers, err := parseInitialNumbers(lines)
 	if err != nil {
-		fmt.Printf("failed to parse ages: %s\n", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	// fmt.Printf("Fish Count (After Day 18): %d\n", fishCountAfterDay(18, ages))
-	// fmt.Printf("Fish Count (After Day 80): %d\n", fishCountAfterDay(80, ages))
-	fmt.Printf("Fish Count (After Day 256): %d\n", fishCountAfterDay(256, ages))
+	fmt.Println("Part one:", partOne(initialNumbers))
+	fmt.Println("Part two:", partTwo(initialNumbers))
 }
+
+func partOne(initialNumbers []int) int {
+	return fishOverDays(initialNumbers, 80)
+}
+
+func partTwo(initialNumbers []int) int {
+	return fishOverDays(initialNumbers, 256)
+}
+
+func fishOverDays(initialNumbers []int, days int) int {
+	clock := [9]int{}
+	for _, n := range initialNumbers {
+		clock[n] = clock[n] + 1
+	}
+
+	for i := 0; i < days; i++ {
+		oldClock := clock
+		clock = [9]int{}
+		for day := range oldClock {
+			if day == 0 {
+				clock[6] += oldClock[day]
+				clock[8] += oldClock[day]
+				continue
+			}
+
+			clock[day-1] += oldClock[day]
+		}
+	}
+
+	totalFish := 0
+	for _, daySum := range clock {
+		totalFish += daySum
+	}
+
+	return totalFish
+}
+
+func parseInitialNumbers(lines []string) ([]int, error) {
+	if len(lines) != 1 {
+		return nil, errors.New("expected one line of input")
+	}
+	parts := strings.Split(lines[0], ",")
+	numbers := []int{}
+	for _, numberString := range parts {
+		n, err := strconv.Atoi(numberString)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, int(n))
+	}
+	return numbers, nil
+}
+
+/*
+   Starting point: 3
+   After 18d, this would generate 3 new fish
+
+   18 - 3 = 15d -- +1 fish
+
+   15 / 6 = 2.5 = 2 -- +2 fish
+
+   Starting point: 4
+   After 18d, this would generate 3 new fish
+
+   18 - 4 = 14d  -- +1 fish
+   14 / 6 = 2.3 = 2 -- +2 fish
+
+   Starting point: 3
+   After 18d, this would generate 3 new fish
+
+   18 - 3 = 15d -- +1 fish
+
+   15 / 6 = 2.5 = 2 -- +2 fish
+
+   Starting point: 1
+   After 18d, this would generate 3 new fish
+
+   18 - 1 = 17d -- +1 fish
+
+   17 / 6 = 2.83 = 2 -- +2 fish
+
+   Starting point: 2
+   After 18d, this would generate 3 new fish
+
+   18 - 2 = 16d -- +1 fish
+
+   16 / 6 = 2.6 = 2 -- +2 fish
+
+   ---
+
+   What's the freakin' formulaaaa?
+
+   1 fish starting at 3
+   18 - 3 = 15 -- +1 fish
+   15 - 6 = 9 -- +1 fish
+   9 - 6 = 3 -- +1 fish
+   3 - 6 = -6 -- +0 fish
+
+   floor(15 / 6) = 2
+   2 * (15/8) = 4
+*/
